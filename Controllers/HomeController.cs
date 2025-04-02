dadbc9365889c5465af400b4102b290a3b791a18
Mishie Mangarateke <ST10371902@vcconnect.edu.za>
2025-04-02 11:12:44 +0200

    Second Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCV.Models;
+
+namespace OnlineCV.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Experience()
+    {
+        return View();
+    }
+
+    public IActionResult Education()
+    {
+        return View();
+    }
+
+    public IActionResult Skills()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
